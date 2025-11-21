043b6ed773613bbf228aafa6a7b9c7a954799283
Vladislav <vladislavprokhorov1005@gmail.com>
2025-11-21 15:35:12 +0200

    fix: small bugs

--- src/Components/FinishScreen/FinishScreen.jsx
+++ src/Components/FinishScreen/FinishScreen.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function FinishScreen({ questions, answers, timeSpent }) {
   const correctCount = answers.filter(
-    (a, i) => a === questions[i].answers
+    (a, i) => a === questions[i].answer
   ).length;
   return (
     <div className="min-h-screen flex flex-col items-center justify-center  bg-gray-100 p-6">
